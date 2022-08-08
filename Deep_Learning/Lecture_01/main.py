92fb849dc855c069d64d1721f02ad81b35e9c0c6
Therkelsen <37107686+Therkelsen@users.noreply.github.com>
2022-08-08 14:12:26 +0200

    Lecture 1 exercises done
    Introduction to numpy, pandas and matplotlib
    
    Co-Authored-By: Simon Vinkel <80058249+SimonCV@users.noreply.github.com>

---
+++ Deep_Learning/Lecture_01/main.py
@@ -0,0 +1,62 @@
+import numpy as np
+import pandas as pd
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+if __name__ == '__main__':
+    #exercise 1
+    a = np.full((2, 3), 4)
+    b = np.array([[1, 2, 3], [4, 5, 6]])
+    c = np.eye(2, 3)
+    d = a + b + c
+
+    print('\na: \n', a)
+    print('\nb: \n', b)
+    print('\nc: \n', c)
+    print('\nd: \n', d)
+
+    a = np.array([[1, 2, 3, 4, 5],
+                  [5, 4, 3, 2, 1],
+                  [6, 7, 8, 9, 0],
+                  [0, 9, 8, 7, 6]])
+
+    print('\nsum of rows of a:\n', a.sum(axis = 1))
+    print('\ntranspose of a:\n', a.transpose())
+
+    #exercise 2
+    print('data read from auto.csv')
+    df = pd.read_csv('auto.csv')
+    print(df.to_string())
+
+    df = df[df['mpg'] >= 16]
+    print('cars with mpg < 16 removed')
+    print(df[df['mpg'] < 16])
+
+    print(df[0:7][['weight', 'acceleration']])
+
+    df = df[df['horsepower'] != '?']
+    print('cars with a ? in horsepower removed')
+    print(df[df['horsepower'] == '?'])
+
+    print('datatype of horsepower column changed to int32')
+    df['horsepower'] = df['horsepower'].astype(int)
+    print(df.dtypes)
+    
+    print(df[1:7].mean())
+
+    #exercise 3
+    a = np.array([1, 1, 2, 3, 5, 8, 13, 21, 34])
+    b = np.array([1, 8, 28, 56, 70, 56, 28, 8, 1])
+
+    data = [a,
+            b]
+
+    plt.plot(data[0], label='training accuracy')
+    plt.plot(data[1], label='validation accuracy')
+    plt.legend(loc='best')
+    plt.xlabel('epocs')
+    plt.ylabel('accuracy')
+    plt.title('Training and validation accuracy')
+    plt.show(block=False)
+    plt.close()
+
