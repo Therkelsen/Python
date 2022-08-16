a948241bdb6b754b544e2c0c0317274a697e9d83
Therkelsen <37107686+Therkelsen@users.noreply.github.com>
2022-08-16 21:46:01 +0200

    changed things up, now at 0.92 acc

--- Deep_Learning/Lecture_04/main.py
+++ Deep_Learning/Lecture_04/main.py
@@ -42,7 +42,7 @@ if __name__ == '__main__':
     print('test_data: X=%s, y=%s' % (test_data.shape, test_labels.shape))
 
     num_classes = 10
-    num_epochs = 100
+    num_epochs = 10
     num_batches = 2 ** 10
     train_labels = to_categorical(train_labels, num_classes)
     test_labels = to_categorical(test_labels, num_classes)
@@ -84,8 +84,8 @@ if __name__ == '__main__':
     history = model.fit(x=train_data, y=train_labels,  # Input and desired outcome
                         batch_size=num_batches,  # Number of samples per gradient update. If none, it defaults to 32
                         epochs=num_epochs,  # Number of runs over the complete x and y
-                        verbose=2,  # Verbosity mode. 0 = silent, 1 = progress bar, 2 = one line per epoch.
-                        callbacks=[PlotLossesKeras()],  # List of functions to call during training
+                        verbose=1,  # Verbosity mode. 0 = silent, 1 = progress bar, 2 = one line per epoch.
+                        callbacks=None,  # List of functions to call during training
                         validation_split=0.0,  # Part of dataset set aside for validating
                         validation_data=(test_data, test_labels),  # Validation dataset, tuple (x_val, y_val)
                         shuffle=True,  # shuffle the training data before each epoch
@@ -94,32 +94,32 @@ if __name__ == '__main__':
                         )
     print('model fitted')
 
-    # history_dict = history.history
-    # loss_values = history_dict['loss']
-    # val_loss_values = history_dict['val_loss']
-    # acc_values = history_dict['accuracy']
-    # val_acc_values = history_dict['val_accuracy']
-    # epochs = range(1, len(loss_values) + 1)
-    # # ’bo’ is for blue dot, ‘b’ is for solid blue line
-    #
-    # plt.subplots(1, 2)
-    # plt.subplot(1, 2, 1)
-    # plt.plot(epochs, loss_values, 'r-', label='Training loss', alpha=0.6)
-    # plt.plot(epochs, val_loss_values, 'm-', label='Validation loss', alpha=0.6)
-    # plt.title('Training and validation loss')
-    # plt.xlabel('Epochs')
-    # plt.ylabel('Loss')
-    # plt.legend()
-    #
-    # plt.subplot(1, 2, 2)
-    # plt.plot(epochs, acc_values, 'g-', label='Training acc', alpha=0.6)
-    # plt.plot(epochs, val_acc_values, 'y-', label='Validation acc', alpha=0.6)
-    # plt.title('Training and validation accuracy')
-    # plt.xlabel('Epochs')
-    # plt.ylabel('Accuracy')
-    # plt.legend()
-    #
-    # plt.show()
+    history_dict = history.history
+    loss_values = history_dict['loss']
+    val_loss_values = history_dict['val_loss']
+    acc_values = history_dict['accuracy']
+    val_acc_values = history_dict['val_accuracy']
+    epochs = range(1, len(loss_values) + 1)
+    # ’bo’ is for blue dot, ‘b’ is for solid blue line
+
+    plt.subplots(1, 2)
+    plt.subplot(1, 2, 1)
+    plt.plot(epochs, loss_values, 'r-', label='Training loss', alpha=0.6)
+    plt.plot(epochs, val_loss_values, 'm-', label='Validation loss', alpha=0.6)
+    plt.title('Training and validation loss')
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.legend()
+
+    plt.subplot(1, 2, 2)
+    plt.plot(epochs, acc_values, 'g-', label='Training acc', alpha=0.6)
+    plt.plot(epochs, val_acc_values, 'y-', label='Validation acc', alpha=0.6)
+    plt.title('Training and validation accuracy')
+    plt.xlabel('Epochs')
+    plt.ylabel('Accuracy')
+    plt.legend()
+
+    plt.show()
 
     model.summary()
 
--- Deep_Learning/test/main.py
+++ Deep_Learning/test/main.py
@@ -1,11 +1,11 @@
 from tensorflow.keras.utils import to_categorical
+from tensorflow.keras import layers
 from tensorflow.keras import optimizers
 from tensorflow.keras import regularizers
 from tensorflow.keras.models import load_model
 from tensorflow.keras.models import Model
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
-from tensorflow.keras.datasets import mnist, fashion_mnist
 from livelossplot import PlotLossesKeras
+import tensorflow.keras.datasets as ds
 import tensorflow as tf
 import seaborn as sns
 import matplotlib.pyplot as plt
@@ -13,10 +13,9 @@ import random
 import numpy as np
 
 if __name__ == '__main__':
-    print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
     # Loads the data
     # (train_data, train_labels), (test_data, test_labels) = ds.mnist.load_data()
-    (train_data, train_labels), (test_data, test_labels) = fashion_mnist.load_data()
+    (train_data, train_labels), (test_data, test_labels) = ds.fashion_mnist.load_data()
     print('data loaded')
     # Plots a single digit from the data
     # summarize loaded dataset
@@ -36,7 +35,7 @@ if __name__ == '__main__':
     plt.show(block=False)
 
     num_classes = 10
-    num_epochs = 50
+    num_epochs = 10
     num_batches = 2 ** 7
     input_shape = (28, 28, 1)
 
@@ -58,24 +57,26 @@ if __name__ == '__main__':
     print('test_data shape:', test_data.shape)
 
     model = tf.keras.Sequential()
-    model.add(Conv2D(2 ** 4, kernel_size=(5, 5), activation='selu', padding="same",
-                     kernel_regularizer=regularizers.L2(l2=1e-2), input_shape=(28, 28, 1)))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Conv2D(2 ** 5, kernel_size=(5, 5), activation='selu', padding="same",
-                     kernel_regularizer=regularizers.L2(l2=1e-2)))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Conv2D(2 ** 5, kernel_size=(5, 5), activation='selu', padding="same",
-                     kernel_regularizer=regularizers.L2(l2=1e-2)))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Flatten())
-    model.add(Dropout(0.5))
-    model.add(Dense(2 ** 6, activation='selu',
-              kernel_regularizer=regularizers.L2(l2=1e-4)))
-    model.add(Dense(10, activation='softmax'))
+    model.add(layers.Input(shape=input_shape))
+
+    model.add(layers.Conv2D(2 ** 5, kernel_size=(3, 3), activation='selu',
+                            kernel_regularizer=regularizers.L2(l2=1e-3)))
+
+    model.add(layers.MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(layers.Conv2D(2 ** 4, kernel_size=(3, 3), activation='selu',
+                            kernel_regularizer=regularizers.L2(l2=1e-3)))
+
+    model.add(layers.MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(layers.Flatten())
+
+    model.add(layers.Dense(num_classes, activation='softmax'))
+
     print('layers added')
 
     # For a multi-class classification problem
-    model.compile(optimizer=optimizers.Adam(learning_rate=0.001),
+    model.compile(optimizer=optimizers.Adam(),
                   loss=tf.keras.losses.CategoricalCrossentropy(),
                   metrics=['Accuracy'])
     print('model compiled')
@@ -83,8 +84,8 @@ if __name__ == '__main__':
     history = model.fit(x=train_data, y=train_labels,  # Input and desired outcome
                         batch_size=num_batches,  # Number of samples per gradient update. If none, it defaults to 32
                         epochs=num_epochs,  # Number of runs over the complete x and y
-                        verbose=2,  # Verbosity mode. 0 = silent, 1 = progress bar, 2 = one line per epoch.
-                        callbacks=[PlotLossesKeras()],  # List of functions to call during training
+                        verbose=1,  # Verbosity mode. 0 = silent, 1 = progress bar, 2 = one line per epoch.
+                        callbacks=None,  # List of functions to call during training
                         validation_split=0.0,  # Part of dataset set aside for validating
                         validation_data=(test_data, test_labels),  # Validation dataset, tuple (x_val, y_val)
                         shuffle=True,  # shuffle the training data before each epoch
@@ -93,35 +94,32 @@ if __name__ == '__main__':
                         )
     print('model fitted')
 
-    # history_dict = history.history
-    # loss_values = history_dict['loss']
-    # val_loss_values = history_dict['val_loss']
-    # acc_values = history_dict['Accuracy']
-    # val_acc_values = history_dict['val_Accuracy']
-    # epochs = range(1, len(loss_values) + 1)
-    # # 'bo' is for blue dot, 'b' is for solid blue line
-    #
-    # plt.subplots(1, 2)
-    # plt.subplot(1, 2, 1)
-    # plt.plot(epochs, loss_values, 'r-', label='Training loss', alpha=0.6)
-    # plt.plot(epochs, val_loss_values, 'm-', label='Validation loss', alpha=0.6)
-    # plt.title('Training and validation loss')
-    # plt.xlabel('Epochs')
-    # plt.ylabel('Loss')
-    # plt.legend()
-    #
-    # plt.subplot(1, 2, 2)
-    # plt.plot(epochs, acc_values, 'g-', label='Training acc', alpha=0.6)
-    # plt.plot(epochs, val_acc_values, 'y-', label='Validation acc', alpha=0.6)
-    # plt.title('Training and validation accuracy')
-    # plt.xlabel('Epochs')
-    # plt.ylabel('Accuracy')
-    # plt.legend()
-    #
-    # plt.subplots_adjust(wspace=0.5,
-    #                     hspace=0.5)
-    #
-    # plt.show(block=False)
+    history_dict = history.history
+    loss_values = history_dict['loss']
+    val_loss_values = history_dict['val_loss']
+    acc_values = history_dict['accuracy']
+    val_acc_values = history_dict['val_accuracy']
+    epochs = range(1, len(loss_values) + 1)
+    # 'bo' is for blue dot, 'b' is for solid blue line
+
+    plt.subplots(1, 2)
+    plt.subplot(1, 2, 1)
+    plt.plot(epochs, loss_values, 'r-', label='Training loss', alpha=0.6)
+    plt.plot(epochs, val_loss_values, 'm-', label='Validation loss', alpha=0.6)
+    plt.title('Training and validation loss')
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.legend()
+
+    plt.subplot(1, 2, 2)
+    plt.plot(epochs, acc_values, 'g-', label='Training acc', alpha=0.6)
+    plt.plot(epochs, val_acc_values, 'y-', label='Validation acc', alpha=0.6)
+    plt.title('Training and validation accuracy')
+    plt.xlabel('Epochs')
+    plt.ylabel('Accuracy')
+    plt.legend()
+
+    plt.show()
 
     model.summary()
 
